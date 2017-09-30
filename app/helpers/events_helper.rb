a1fcb3ed88a550622c042354972b3baba298a9a5
Justacode <mustafinkzn97@gmail.com>
2017-09-30 18:33:36 +0300

    Initial

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,6 @@
 module EventsHelper
 	def day_events
 		date = params[:day] ? params[:day].to_date : @date
-		@calendar[@date] unless @date == params[:date]
+		@calendar[date] unless @date == params[:date]
 	end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,7 @@ class Event < ApplicationRecord
 	validates :frequency, inclusion: { in: %w(once dayly weekly monthly yearly)}
 
 	def self.by_range (start_date, finish_date)
-		self.where.not("finish_date < ? OR start_date > ?", start_date, finish_date).to_a
+		self.where("finish_date > ? AND start_date < ?", start_date, finish_date).to_a
 	end
 
 end
